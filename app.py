519f8b9a12434baaa9f38d92f68f2576ae1fa91b
ehhhh <39893695+luo-jinzhang@users.noreply.github.com>
2019-12-01 19:57:44 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+# app.py
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+# @asyncio.coroutine
+async def init(loop):
+    app = web.Application()
+    app.router.add_route('GET','/',index)
+    # srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '1227.0.0.1',9000)
+    await site.start()
+    logging.info('server started at http://127.0.0.1:9000...')
+    #return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
